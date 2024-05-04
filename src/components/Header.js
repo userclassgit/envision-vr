7b7ae7fdae3d88aaae4c28d6b221504d425baad3
Myeongjeon Lee <userclassdev@outlook.com>
2024-05-03 20:49:24 -0500

    add header-text, about to make the button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,10 @@ const Header = () => {
       <video src={heroVid} autoPlay loop muted></video>
       <div className="container header-content">
         <HeaderTop />
+        <div className="header-text">
+          <h1>Immerse yourself in a new reality with EnvisionVR.</h1>
+          <p>We make the best VR headsets in the world.</p>
+        </div>
       </div>
     </header>
   );
