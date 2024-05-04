f966b021cbf8e0c351417b3837ada9a6662e6333
Myeongjeon Lee <userclassdev@outlook.com>
2024-05-03 22:09:26 -0500

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './css/app.css';
 import Header from './components/Header';
 import Gallery from './components/Gallery';
+import Footer from './components/Footer';
 
 /* 
 I prefer assigning the component arrow function to a const like this instead of writing a named function.
@@ -18,6 +19,7 @@ const App = () => {
           <Gallery />
         </div>
       </section>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import facebookIcon from '../media/social-facebook.png';
+import instagramIcon from '../media/social-instagram.png';
+import youtubeIcon from '../media/social-youtube.png';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="socials">
+          <img src={facebookIcon} alt="Facebook" />
+          <img src={instagramIcon} alt="Instagram" />
+          <img src={youtubeIcon} alt="YouTube" />
+        </div>
+        <span>©EnvisionVR</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
